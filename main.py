8a54f77cd0f0a96b546ecb67bd2feb1f744fd6ff
RJjulia <juliazzkx@gmail.com>
2023-05-21 11:12:39 -0300

    adicionando botões

--- main.py
+++ main.py
@@ -23,14 +23,31 @@ frame_body = Frame(janela, width=235, height=268)
 frame_body.grid(row=1, column=0)
 
 #criando os botoes
-b1 = Button(frame_body, text="C", width=11, height=2, bg=cor3, font=('Ivy 13q bold'), relief=RAISED)
+b1 = Button(frame_body, text="C", width=11, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b1.place(x=0, y=0)
-
-b2 = Button(frame_body, text="%", width=5, height=2, bg=cor3)
-b2.place(x=90, y=0)
-
-b3 = Button(frame_body, text="%", width=5, height=2, bg=cor4,fg=cor5)
+b2 = Button(frame_body, text="%", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b2.place(x=118, y=0)
+b3 = Button(frame_body, text="/", width=5, height=2, bg=cor4,fg=cor5, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b3.place(x=177, y=0)
+
+b4 = Button(frame_body, text="7", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b4.place(x=0, y=52)
+b5 = Button(frame_body, text="8", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b5.place(x=59, y=52)
+b6 = Button(frame_body, text="9", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b6.place(x=118, y=52)
+b7 =  Button(frame_body, text="*", width=5, height=2, bg=cor4,fg=cor5, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b7.place(x=177, y=52)
+
+b4 = Button(frame_body, text="4", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b4.place(x=0, y=52)
+b5 = Button(frame_body, text="5", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b5.place(x=59, y=52)
+b6 = Button(frame_body, text="6", width=5, height=2, bg=cor3, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b6.place(x=118, y=52)
+b7 =  Button(frame_body, text="-", width=5, height=2, bg=cor4,fg=cor5, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b7.place(x=177, y=52)
+
 
 janela.mainloop()
 
